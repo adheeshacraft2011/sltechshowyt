db51954d8852e5ca2c977a305e9ffd3ec743aa36
adheeshacraft2011 <138423445+adheeshacraft2011@users.noreply.github.com>
2023-07-08 09:27:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hi Guys")
+
+// this is a comment
+
+/* This is a multi line comment 
+this is the second line
+this is the third line
+this is the last line\
+*/
+var x=3;
+console.log(x)
+
+ x=5;
+console.log(x)
+
+ x=x+1;
+console.log (x)
+
+ x++
+console.log(x)
